0b697dcfb39d64107c89900522401526d18c686c
mohammadkhalid962 <mohammadkhalid962@gmail.com>
2016-05-07 17:04:47 +0000

    Updated App

--- routes.js
+++ routes.js
@@ -1,4 +1,4 @@
-// Local deps
+// Local dependencies
 var controllers = require('./controllers');
 
 var Router = {
@@ -7,13 +7,13 @@ var Router = {
 
         // Home page
         app.get('/', controllers.Home.index);
-        
+
         // About page
         app.get('/about', controllers.About.index);
 
         // Users
         app.get('/users', controllers.Users.index);
-        
+
     }
 
 };
