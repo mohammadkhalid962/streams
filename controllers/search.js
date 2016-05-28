465f9758561297a74215fbd5da3222134b9e0a05
mohammadkhalid962 <mohammadkhalid962@gmail.com>
2016-05-28 18:50:40 +0000

    Add varaiables to search templete

--- controllers/search.js
+++ controllers/search.js
@@ -1,8 +1,16 @@
 // Search Controller
 var Search = {
-    
-    index: function( req, res){
+    index: function(req, res){
         res.render('search/index');
+    },
+    
+    search: function( req, res, next){
+        var data = {
+            items: [0, 1, 2, 3, 4, 5],
+            numItems: 6,
+            searchTerm: req.body.hashtag
+        };
+        res.render('search/index', data);
     }
 };
 
--- routes.js
+++ routes.js
@@ -13,6 +13,7 @@ var Router = {
         
         // Search
         app.get('/search', controllers.Search.index);
+        app.post('/search', controllers.Search.search);
     }
 
 };
