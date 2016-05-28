3f752e411e778ed99b8e0627d18d39010701fbd9
mohammadkhalid962 <mohammadkhalid962@gmail.com>
2016-05-28 19:49:21 +0000

    Using twitter results for counts

--- controllers/search.js
+++ controllers/search.js
@@ -1,17 +1,38 @@
 // Search Controller
+
+// local dependencies
+
+var Helpers = require('../helpers');
 var Search = {
     index: function(req, res){
         res.render('search/index');
     },
     
     search: function( req, res, next){
-        var data = {
-            items: [0, 1, 2, 3, 4, 5],
-            numItems: 6,
-            searchTerm: req.body.hashtag
+        var hashtag = req.body.hashtag;
+        
+        var client = Helpers.getTwitterClient();
+        
+        var params = {
+            q: hashtag
         };
+        
+        client.get('search/tweets', params, function(err, tweets) {
+            var data = {
+            searchTerm: hashtag,
+            numItems: 0
+        };
+        
+        if (tweets) {
+            data.items = tweets.statuses;
+            data.numItems = data.items.length;
+        }
         res.render('search/index', data);
+        
+        });
     }
+        
+        
 };
 
 module.exports = Search;
